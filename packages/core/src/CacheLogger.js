613e23a8a65fe79095177b948a26c49c80d2d850
audilin <audilin2005@gmail.com>
2024-10-08 10:36:23 -0400

    cache download button

--- packages/core/src/CacheLogger.js
+++ packages/core/src/CacheLogger.js
@@ -25,7 +25,7 @@ export class CacheLogger {
 
     displayLogsAsTable() {
         const logs = JSON.parse(localStorage.getItem(this.storageKey)) || [];
-        
+
         const formattedLogs = logs.map((log) => ({
             Query: log.query,
             Timestamp: log.timestamp,
@@ -34,6 +34,29 @@ export class CacheLogger {
         }));
 
         console.table(formattedLogs);
+    }
+
+
+    downloadLogs() { // will allow user to download cache.json
+        // same formattedLogs as displayLogsAsTable
+        const logs = JSON.parse(localStorage.getItem(this.storageKey)) || [];
+
+        const formattedLogs = logs.map((log) => ({
+            Query: log.query,
+            Timestamp: log.timestamp,
+            Latency: log.latency,
+            Size: `${log.size} bytes`,
+        }));
+
+        const blob = new Blob([JSON.stringify(formattedLogs, null, 2)], {
+            type: 'application/json',
+        });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = `cache.json`;
+        a.click();
+        URL.revokeObjectURL(url);
     }
 
     clear() {

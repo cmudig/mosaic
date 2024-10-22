845eef89ab6004d764d3c9f57d83a4f05eb79b1f
Allen Luo <yuhualuo@andrew.cmu.edu>
2024-10-22 13:14:32 -0400

    Restructure the files, make the cache analysis more generic, delete unsued files

--- packages/cache_webapp/src/cache-analysis.md
+++ packages/cache_webapp/src/cache-analysis.md
@@ -8,32 +8,50 @@ toc: false
 
 <!-- Allow users to upload Json file -->
 ```js
+import { FileAttachment } from "observablehq:stdlib";
+import { lru_cache } from "./components/lru.js";
 import {
  cache_hit,
- find_max_cache_size,
- lru_cache,
- cache_used_graph,
  cache_hit_graph,
+ cache_repeat_hit_graph,
+ cache_used_graph,
+ find_max_cache_size,
  prase_logs_with_cache
-} from "./components/lru.js";
+} from "./components/cache_analysis_helper.js";
 ```
 <hr>
 
 ### Select/Upload Cache File
 
 ```js
-const jsonfile = view(Inputs.file({label: "Cache Log File", accept: ".json", required: true}));
+let fileList = [
+  FileAttachment("data/test1.json"),
+  FileAttachment("data/test2.json"),
+  FileAttachment("data/test3.json")
+]
+
+const jsonfile = view(Inputs.select(fileList, {
+  format: (d) => d.name,
+  label: "Currently File"
+}));
+
+// const jsonfile = view(Inputs.file({label: "Cache Log File", accept: ".json", required: true}));
 ```
 
 ```js
 let max_cache_size = find_max_cache_size(jsonfile.json());
 ```
 
-<div class="grid grid-cols-1">
+<div class="grid grid-cols-2">
  <div class="card">
    <h2>Cache Size v.s. Hit Rate</h2>
    <br></br>
-   ${resize((width) => cache_hit_graph(jsonfile.json(), max_cache_size, {width}))}
+   ${resize((width) => cache_hit_graph(jsonfile.json(), max_cache_size, lru_cache, {width}))}
+ </div>
+ <div class="card">
+   <h2>Cache Size v.s. Repeated Query Hit Rate</h2>
+   <br></br>
+   ${resize((width) => cache_repeat_hit_graph(jsonfile.json(), max_cache_size, lru_cache, {width}))}
  </div>
 </div>
 
---
+++ packages/cache_webapp/src/components/cache_analysis_helper.js
@@ -0,0 +1,186 @@
+import * as Plot from "npm:@observablehq/plot";
+
+// For each logs from the input, based on the cache add another field inside the 
+// dict to indicate whether the query is hit/miss
+export function prase_logs_with_cache(logs_promise, cache) {
+
+    return logs_promise.then(logs => {
+        return logs.map(log => {
+            if (cache.get(log.Query)) {
+                log.CacheStatus = "Hit"
+            } else {
+                cache.set(log.Query, log, parseInt(log.Size, 10))
+                log.CacheStatus = "Miss"
+            }
+            return log
+        })
+    })
+}
+
+// Calculate the hit ratio for a list of queries and specific cache
+// Note: in this case hit ratio = hit count / total count
+export function cache_hit(logs_promise, cache, limit=null) {
+    
+    return logs_promise.then(logs => {
+        let total_get_queries = 0;
+        let cached_get_queries = 0;
+        for (const log of logs) {
+            if (cache.get(log.Query)) {
+                cached_get_queries += 1;
+            } else {
+                cache.set(log.Query, log, parseInt(log.Size, 10))
+            }
+            total_get_queries += 1;
+        }
+        if (!limit) {
+            return cached_get_queries / total_get_queries
+        } else {
+            return (cached_get_queries / total_get_queries).toFixed(limit)
+        }
+        
+    })
+    
+}
+
+// Calculate the hit ratio (only for repeated queries) for a list of queries
+// and specific cache
+export function cache_repeat_hit(logs_promise, cache, limit=null) {
+    
+    return logs_promise.then(logs => {
+        let total_repeated_queries = 0;
+        let cached_repeated_queries = 0;
+        let seen = new Set();
+        for (const log of logs) {
+            let cached = cache.get(log.Query);
+            if (seen.has(log.Query)) {
+                total_repeated_queries += 1;
+                if (cached) {
+                    cached_repeated_queries += 1;
+                }
+            }
+
+            if (!cached) {
+                cache.set(log.Query, log, parseInt(log.Size, 10))
+            }
+            seen.add(log.Query)
+        }
+
+        if (!limit) {
+            return cached_repeated_queries / total_repeated_queries
+        } else {
+            return (cached_repeated_queries / total_repeated_queries).toFixed(limit)
+        }
+        
+    })
+    
+}
+
+// Calculate the min cache size to achieve best hit rate
+export function find_max_cache_size(logs_promise) {
+
+    return logs_promise.then(logs => {
+        let seen = new Map();
+        let total_size = 0;
+        for (const log of logs) {
+            let size = parseInt(log.Size, 10);
+            if (!seen.get(log.Query)) {
+                seen.set(log.Query, log, size);
+                total_size += size;
+            }
+        }
+        return total_size
+    })
+}
+
+// Generate a graph with x axis as the cache size and y axis as the hit ratio
+export async function cache_hit_graph(jsonfile, size, lru_cache, {width, height} = {}) {
+    let x_axis = Array.from({ length: 11 }, (_, i) => Math.floor((i * size) / 10));
+    let data = await Promise.all(
+        x_axis.map(async (xValue) => {
+          const rate = await cache_hit(jsonfile, lru_cache(xValue));
+          return { x: xValue, y: rate };
+        })
+    );
+      
+    return Plot.plot({
+        width,
+        height,
+        marginTop: 30,
+        x: {
+            label: "Cache Size",
+            nice: true,
+            tickFormat: ".0f",
+          },
+          y: {
+            label: "Cache Hit Rate",
+            nice: true,
+            tickFormat: ".2f",
+          },
+        marks: [
+            Plot.line(data, {x: "x", y: "y", stroke: "steelblue", strokeWidth: 2}),
+            Plot.dot(data, {x: "x", y: "y", r: 5, fill: "red", tip: true}),
+        ]
+    });
+}
+
+// Generate a graph with x axis as the cache size and y axis as the hit ratio (only repeated queries)
+export async function cache_repeat_hit_graph(jsonfile, size, lru_cache, {width, height} = {}) {
+    let x_axis = Array.from({ length: 11 }, (_, i) => Math.floor((i * size) / 10));
+    let data = await Promise.all(
+        x_axis.map(async (xValue) => {
+          const rate = await cache_repeat_hit(jsonfile, lru_cache(xValue));
+          return { x: xValue, y: rate };
+        })
+    );
+      
+    return Plot.plot({
+        width,
+        height,
+        marginTop: 30,
+        x: {
+            label: "Cache Size",
+            nice: true,
+            tickFormat: ".0f",
+          },
+          y: {
+            label: "Cache Hit Rate",
+            nice: true,
+            tickFormat: ".2f",
+          },
+        marks: [
+            Plot.line(data, {x: "x", y: "y", stroke: "steelblue", strokeWidth: 2}),
+            Plot.dot(data, {x: "x", y: "y", r: 5, fill: "red", tip: true}),
+        ]
+    });
+}
+
+// Generate a graph with x axis as the query index and y axis as the % of cache used
+export async function cache_used_graph(jsonfile, cache, {width, height} = {}) {
+    let log_entries = await jsonfile;
+    let data = log_entries.map((log) => {
+        if (!cache.get(log.Query)) {
+            cache.set(log.Query, log, parseInt(log.Size, 10));
+        }
+        return { x: log.Index + 1, y: cache.get_used_size() / cache.get_cache_size()};
+    })
+
+    return Plot.plot({
+        width,
+        height,
+        marginTop: 30,
+        x: {
+            label: "Log Index",
+            nice: true,
+            tickFormat: ".0f",
+        },
+        y: {
+            label: "Cache Occupied Rate",
+            nice: true,
+            tickFormat: ".2f",
+        },
+        marks: [
+            Plot.line(data, {x: "x", y: "y", stroke: "steelblue", strokeWidth: 2}),
+            Plot.dot(data, {x: "x", y: "y", r: 5, fill: "red", tip: true}),
+        ]
+    });
+}

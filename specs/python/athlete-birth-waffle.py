8fc80e35220c31da2134c7d859d8bb0658a68343
sittingthyme <nicholan@andrew.cmu.edu>
2026-03-30 01:49:44 -0400

    Fix Athlete Birth Waffle

--- specs/python/athlete-birth-waffle.py
+++ specs/python/athlete-birth-waffle.py
@@ -1,56 +1,29 @@
-import json
-import mosaic.vgplot as vg
+from mosaic import *
+from mosaic.spec import *
+from mosaic.generated_classes import *
+from typing import Dict, Any, Union
 
-meta = vg.meta(title="Athlete Birth Waffle", description="Waffle chart counting Olympic athletes based on which half-decade they were born. The inputs enable adjustment of waffle mark design options.\n", credit="Adapted from an [Observable Plot example](https://observablehq.com/@observablehq/plot-waffle-unit).")
-data = vg.data(
-    athletes=vg.parquet("data/athletes.parquet")
+
+athletes = DataSource(
+    type="parquet",
+    file="data/athletes.parquet",
+    where=""
 )
 
-view = vg.vconcat(
-    vg.hconcat(
-            vg.input("menu", as_="$unit", options=[
-                1,
-                2,
-                5,
-                10,
-                25,
-                50,
-                100
-            ], label="Unit"),
-            vg.input("menu", as_="$round", options=[
-                True,
-                False
-            ], label="Round"),
-            vg.input("menu", as_="$gap", options=[
-                0,
-                1,
-                2,
-                3,
-                4,
-                5
-            ], label="Gap"),
-            vg.slider(as_="$radius", min=0, max=10, step=0.1, label="Radius")
-        ),
-    {
-        "vspace": 10
-    },
-    vg.plot(
-            vg.waffle_y(data=vg.from_("athletes"), unit="$unit", round="$round", gap="$gap", rx="$radius", x={
-                "sql": "5 * floor(year(\"date_of_birth\") / 5)"
-            }, y={
-                "count": ""
-            }),
-            vg.x_label(None),
-            vg.x_tick_size(0),
-            vg.x_tick_format("d")
-        )
+spec = Plot(
+    plot=[
+        PlotMark(WaffleY(
+            mark="waffleY",
+            data=PlotFrom(from_="athletes"),
+            unit=$unit,
+            round=$round,
+            gap=$gap,
+            rx=$radius,
+            x=ChannelValueSpec(ChannelValue(sql='5 * floor(year("date_of_birth") / 5)')),
+            y=ChannelValueSpec(ChannelValue(count=""))
+        ))
+    ],
+    xLabel=None,
+    xTickSize=0,
+    xTickFormat="d"
 )
-spec = vg.spec(meta=meta, data=data, params={
-    "unit": 10,
-    "round": False,
-    "gap": 1,
-    "radius": 0
-}, view=view)
-
-if __name__ == "__main__":
-    print(json.dumps(spec.to_dict(), sort_keys=True))
